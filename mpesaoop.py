6c692faeabb6b2718d56503a99d1702457ad76f4
Johncarsons <thejohncarsons@gmail.com>
2025-02-11 09:03:05 +0300

    initial commit

--- mpesaoop.py
+++ mpesaoop.py
@@ -8,11 +8,16 @@ class Account:
         self.balance = balance
     def deposit(self, amount):
         self.balance += amount #if balance
+        print(f"Deposit of {amount} to {self.holders_name} is successful. New balance is {self.balance}")
+
+
     def withdraw(self, amount):
+        print(f"Attemoting to withdraaw {amount} from account with  {self.balance} as balance.")
         if amount <= self.balance:
             self.balance -= amount #subtract from balance
+            print(f"Withdrawal of {amount} successful. Account balance is now {self.balance}")
         else:
-            print("Insufficient balance")
+            print(f"Insufficient balance. Account balance is {self.balance} ")
     def get_balance(self):
         return self.balance
 
@@ -58,7 +63,7 @@ deposit1.execute(account1)
 withdraw1.execute(account1)
 print(f"The balance of {account1.get_holders_name()} is {account1.get_balance()}")
 
-account2 = customer( 56789, "Kiama", 00, 722357224)
+account2 = customer( 56789, "Kiama", 123456789, 722357224)
 deposit2 = DepositTransaction(560)
 withdraw2 = WithdrawTransaction(910000)
 deposit2.execute(account2)
